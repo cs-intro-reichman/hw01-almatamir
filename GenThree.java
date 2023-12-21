178711546f2bffe1ecdbeb9022b5015fc74253cd
alma tamir <alma.tamir@runi.ac.il>
2023-12-21 21:52:44 +0200

    genthree

--- GenThree.java
+++ GenThree.java
@@ -6,13 +6,13 @@ public class GenThree {
 	public static void main(String[] args) {
 	  int a =  Integer.parseInt(args[0]);
       int b = Integer.parseInt(args[1]);
-      int min1= Math.min(a,b);
+      int min= Math.min(a,b);
       int max= Math.max(a,b);
-      int r = (int) (Math.random()*(max - min1) + min1);
-      int r2 = (int) (Math.random()*(max - min1) + min1);
-      int r3 = (int) (Math.random()*(max - min1) + min1);
-      int min= Math.min(r,Math.min(r2,r3));
-      System.out.println(r+" "+r2+" "+r3+" "+min);
+      int r = (int) (Math.random()*(max - min) + min);
+      int r2 = (int) (Math.random()*(max - min) + min);
+      int r3 = (int) (Math.random()*(max - min) + min);
+      int minr= Math.min(r,Math.min(r2,r3));
+      System.out.println("for input string: "+r+" "+r2+" "+r3+" "+minr);
       
 
 	}
