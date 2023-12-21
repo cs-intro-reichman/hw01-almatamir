6770b5e0a44cc65f97a7e8316a486c705dd9e998
alma tamir <alma.tamir@runi.ac.il>
2023-12-21 18:56:51 +0200

    fix l

--- LinearEq.java
+++ LinearEq.java
@@ -5,6 +5,7 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
+    public static void main(String[] args) {
 	  double a = Integer.parseInt(args[0]);
       double b = Integer.parseInt(args[1]);
       double c = Integer.parseInt(args[2]);
@@ -13,4 +14,5 @@ public class LinearEq {
       x=save/a;
       System.out.println(a+"x + "+b+" = "+c);
       System.out.println("x is: "+x);
+  }
 }
